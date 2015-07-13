c5996f8cb71183926fc60ae301d97284615926b5
opopi123 <royjoo@buffalo.edu>
2015-07-13 16:25:17 -0400

    commit for secret
    added all boolean variables that hold triggers

--- src/GUI/Secret.java
+++ src/GUI/Secret.java
@@ -2,4 +2,33 @@ package GUI;
 
 public class Secret {
 
-}
+	public Boolean trigger1;
+	public Boolean trigger2;
+	public Boolean trigger3;
+	public Boolean trigger4;
+	public Boolean trigger5;
+	public Boolean trigger6;
+	public Boolean trigger7;
+	public Boolean trigger8;
+	public Boolean trigger9;
+	public Boolean trigger10;
+	public Boolean trigger11;
+
+	Secret( Boolean t1, Boolean t2, Boolean t3, Boolean t4, Boolean t5, Boolean t6, Boolean t7, Boolean t8, Boolean t9, Boolean t10, Boolean t11){
+			this.trigger1 = t1;
+			this.trigger2 = t2;
+			this.trigger3 = t3;
+			this.trigger4 = t4;
+			this.trigger5 = t5;
+			this.trigger6 = t6;
+			this.trigger7 = t7;
+			this.trigger8 = t8;
+			this.trigger9 = t9;
+			this.trigger10 = t10;
+			this.trigger11 = t11;
+			
+			
+		}
+	}
+
+
