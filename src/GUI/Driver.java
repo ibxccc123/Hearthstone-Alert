028c96af86b7663b8daacd41ac6356acc9862046
ibxccc123 <ibxccc123@gmail.com>
2015-07-13 15:15:38 -0400

    Secret Class

---
+++ src/GUI/Driver.java
@@ -0,0 +1,27 @@
+package GUI;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Driver {
+	public static void main (String [] args) {
+
+		
+	
+	
+//		JFrame frame = new JFrame("Show her Natasha");
+//		JPanel panel = new JPanel();
+//		frame.add(panel);
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		frame.pack();
+//		frame.setVisible(true);
+//		frame.setSize(500, 250);
+//		JButton button = new JButton("Cliiiiiiiick meeeeeeee ::::3");
+//		button.addActionListener(new Events());
+//		frame.add(button);
+
+//}
+
+	}
+}
