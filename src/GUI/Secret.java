5f77dcc4556db2ecb9bad557582ce07a62b88595
opopi123 <royjoo@buffalo.edu>
2015-07-16 17:19:23 -0400

    Added Functions
    Added functions to Secret and SecretArray.java

--- src/GUI/Secret.java
+++ src/GUI/Secret.java
@@ -5,8 +5,10 @@ public class Secret {
 	//Changed it to conserve room
 	public Boolean trigger1, trigger2, trigger3, trigger4, trigger5, trigger6,
     trigger7, trigger8, trigger9, trigger10, trigger11;
+	public Boolean value;
+	public String SecretName;
 
-	Secret( Boolean t1, Boolean t2, Boolean t3, Boolean t4, Boolean t5, Boolean t6, Boolean t7, Boolean t8, Boolean t9, Boolean t10, Boolean t11){
+	Secret( String SecretN, Boolean t1, Boolean t2, Boolean t3, Boolean t4, Boolean t5, Boolean t6, Boolean t7, Boolean t8, Boolean t9, Boolean t10, Boolean t11){
 			this.trigger1 = t1;	// M -> EM
 			this.trigger2 = t2;	// M -> EH
 			this.trigger3 = t3;	// H -> EM
@@ -18,9 +20,22 @@ public class Secret {
 			this.trigger9 = t9;	// S
 			this.trigger10 = t10;	// S ->  EM 
 			this.trigger11 = t11;	// S -> EH
-			
+			this.SecretName = SecretN;
+			this.value = true;
 			
 		}
+	public Boolean isValid(){
+		return this.value;
 	}
+	
+	public void setToFalse(){
+		this.value = false;
+	}
+	
+	public String getSecretName(){
+		return this.SecretName;
+		
+	}
+}
 
 
--- src/GUI/SecretArray.java
+++ src/GUI/SecretArray.java
@@ -2,6 +2,10 @@ package GUI;
 
 public class SecretArray {
 
+	public Secret[] hunterarray;
+	public Secret[] magearray;
+	public Secret[] pallyarray;
+	
 	/* hunter is 1
 	 * mage is 2
 	 * palladin is 3
@@ -9,14 +13,13 @@ public class SecretArray {
 	SecretArray(int classnum){
 		switch(classnum){
 		case 1:
-			Secret[] hunterarray;
 			hunterarray = new Secret[5];
 			
-    		Secret freezing = new Secret(true,true,false,false,false,false,false,false,false,false,false);
-    		Secret explosive = new Secret(false,true,false,true,false,false,false,false,false,false,false);
-    		Secret snipe = new Secret(false,false,false,false,true,false,false,false,false,false,false);
-    		Secret misdirection = new Secret(false,true,false,true,false,false,false,false,false,false,false);
-    		Secret snake = new Secret(true,false,true,false,false,false,false,false,false,false,false);
+    		Secret freezing = new Secret("Freezing Trap",true,true,false,false,false,false,false,false,false,false,false);
+    		Secret explosive = new Secret("Explosive Trap",false,true,false,true,false,false,false,false,false,false,false);
+    		Secret snipe = new Secret("Snipe", false,false,false,false,true,false,false,false,false,false,false);
+    		Secret misdirection = new Secret("Misdirection",false,true,false,true,false,false,false,false,false,false,false);
+    		Secret snake = new Secret("Snake Trap", true,false,true,false,false,false,false,false,false,false,false);
     		hunterarray[0] = freezing;
     		hunterarray[1] = explosive;
     		hunterarray[2] = snipe;
@@ -24,15 +27,14 @@ public class SecretArray {
     		hunterarray[4] = snake;
     		break;
 		case 2:
-			Secret[] magearray;
 			magearray = new Secret[7];
-        	Secret duplicate = new Secret(false,false,false,false,false,true,false,false,false,false,false);
-    		Secret ice_barrier = new Secret(false,true,false,true,false,false,false,false,false,false,false);
-    		Secret mirror_entity = new Secret(false,false,false,false,true,false,false,false,false,false,false);
-    		Secret counterspell = new Secret(false,false,false,false,false,false,false,false,true,false,false);
-    		Secret vaporize = new Secret(false,true,false,false,false,false,false,false,false,false,false);
-    		Secret ice_block = new Secret(false,false,false,false,false,false,true,false,false,false,false);
-    		Secret spellbender = new Secret(false,false,false,false,false,false,false,false,false,true,false);
+        	Secret duplicate = new Secret("Duplicate", false,false,false,false,false,true,false,false,false,false,false);
+    		Secret ice_barrier = new Secret("Ice Barrier", false,true,false,true,false,false,false,false,false,false,false);
+    		Secret mirror_entity = new Secret("Mirror Entity", false,false,false,false,true,false,false,false,false,false,false);
+    		Secret counterspell = new Secret("CounterSpell", false,false,false,false,false,false,false,false,true,true,true);
+    		Secret vaporize = new Secret("Vaporize", false,true,false,false,false,false,false,false,false,false,false);
+    		Secret ice_block = new Secret("Ice Block", false,false,false,false,false,false,true,false,false,false,false);
+    		Secret spellbender = new Secret("Spellbender", false,false,false,false,false,false,false,false,false,true,false);
     		magearray[0] = duplicate;
     		magearray[1] = ice_barrier;
     		magearray[2] = mirror_entity;
@@ -42,13 +44,12 @@ public class SecretArray {
     		magearray[6] = spellbender;
     		break;
 		case 3:
-			Secret[] pallyarray;
 			pallyarray = new Secret[5];
-        	Secret avenge = new Secret(false,false,false,false,false,false,false,true,false,false,false);
-    		Secret eye = new Secret(false,true,false,true,false,false,false,false,false,false,true);
-    		Secret noble_sacrifice = new Secret(true,true,true,true,false,false,false,false,false,false,false);
-    		Secret redemption = new Secret(false,false,false,false,false,true,false,false,false,false,false);
-    		Secret repentance = new Secret(false,false,false,false,true,false,false,false,false,false,false);
+        	Secret avenge = new Secret("Avenge", false,false,false,false,false,false,false,true,false,false,false);
+    		Secret eye = new Secret("Eye for an Eye", false,true,false,true,false,false,false,false,false,false,true);
+    		Secret noble_sacrifice = new Secret("Noble Sacrifice", true,true,true,true,false,false,false,false,false,false,false);
+    		Secret redemption = new Secret("Redemption", false,false,false,false,false,true,false,false,false,false,false);
+    		Secret repentance = new Secret("Repentance", false,false,false,false,true,false,false,false,false,false,false);
     		pallyarray[0] = avenge;
     		pallyarray[1] = eye;
     		pallyarray[2] = noble_sacrifice;
@@ -62,4 +63,38 @@ public class SecretArray {
 		}
 		
 	}
+	public String ReturnArray(SecretArray sa, int classnum){
+		String s;
+		s = new String();
+		
+		switch(classnum){
+		case 1:
+			for(int i = 0; i > 5; i++){
+				if (sa.hunterarray[i].isValid()){
+					s = s + " " + hunterarray[i].getSecretName();
+				}
+
+					
+				
+			}
+			break;
+		case 2:
+			for(int i = 0; i > 7; i++){
+				if(sa.magearray[i].isValid()){
+					s = s + " " + magearray[i].getSecretName();
+				}
+			}
+			break;
+		case 3:
+			for(int i = 0; i > 5; i++){
+				if(sa.pallyarray[i].isValid()){
+					s = s + " " + pallyarray[i].getSecretName();
+				}
+			}
+			
+		}
+		return s;
+	}
+	
+	
 }
