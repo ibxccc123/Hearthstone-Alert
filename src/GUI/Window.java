27c5234f7fdd882561be0d2f64f4a756d9fa2707
ibxccc123 <ibxccc123@gmail.com>
2015-07-16 09:14:43 -0400

    Added rework() to Window class
    rework() cleans the JFrame and populates it with panels and buttons and
    a JLabel that contains a string of all remaining secrets

--- src/GUI/Window.java
+++ src/GUI/Window.java
@@ -1,10 +1,14 @@
 package GUI;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Window {
@@ -23,7 +27,7 @@ public class Window {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
-		frame.setSize(700, 600);
+		frame.setSize(400, 400);
 		
 		/* moved all lines where the Secrets are instantiated to SecretArray.java
 		 * Instead the actionlisteners just create new SecretArrays
@@ -33,45 +37,79 @@ public class Window {
 		 * We should discuss if we are making the loops in the actionlisteners or if the ALs call another function next
 		 * 
 		 */
-		
+	
 		JButton hunterButton = new JButton("Hunter");
 		hunterButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e){
+            	frame.setSize(400, 500);
             	SecretArray myHunterSecret = new SecretArray(1);
-            	frame.setSize(200, 200);
-            	frame.getContentPane().removeAll();
+            	rework(frame, myHunterSecret);     	//AL's will call another function that can loop upon itself
             }
         });
-		panel1.add(hunterButton);
 		
 		JButton mageButton = new JButton("Mage");
 		mageButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e){
-            	SecretArray myMageSecret = new SecretArray(2);
-            	frame.setSize(200, 200);
-            	frame.getContentPane().removeAll();
+            	frame.setSize(400, 500);
+            	SecretArray myMageSecret = new SecretArray(1);
+            	rework(frame, myMageSecret);     	
             }
-        });
-		panel2.add(mageButton);
-		
+        });	
+
 		JButton pallyButton = new JButton("Paladin");
 		pallyButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e){
-            	SecretArray myPallySecret = new SecretArray(3);
-            	frame.setSize(200, 200);
-            	frame.getContentPane().removeAll();
+            	frame.setSize(400, 500);
+            	SecretArray myPallySecret = new SecretArray(1);
+            	rework(frame, myPallySecret);     	
             }
         });
+		
+		panel1.add(hunterButton);
+		panel2.add(mageButton);
 		panel3.add(pallyButton);
-		
-		
 		//Adds the buttons to the JFrame, with the panels they're each in.
+		frame.setLayout(new GridLayout(2, 2));
 		frame.getContentPane().add(panel1, BorderLayout.CENTER);
 		frame.getContentPane().add(panel2, BorderLayout.LINE_START);
-		frame.getContentPane().add(panel3, BorderLayout.LINE_END);
-		
-
+		frame.getContentPane().add(panel3, BorderLayout.LINE_END);	
 		
 	}
-
+	
+	//Method that will loop upon itself to refresh the window every time it's called
+	//Should remove elements of SecretArray every time a button is pressed
+	//Define buttons' ActionListeners in here, or through another method
+	public void rework(JFrame frame, SecretArray secrets){
+		
+    	frame.getContentPane().removeAll();
+    	JPanel[] panelArray = new JPanel[20]; 
+    	JButton[] buttonArray = new JButton[20];
+    	for(int i=0; i<11; i++){  //Creating panels/buttons for triggers, 11 overall
+    		panelArray[i] = new JPanel();
+    		buttonArray[i] = new JButton("Placeholder");
+    		buttonArray[i].addActionListener(new ActionListener() {
+    			public void actionPerformed(ActionEvent e){
+    				
+    			}
+    		});
+    		panelArray[i].add(buttonArray[i]);
+    	}
+    	//12th panel is a JLabel for the text of what secrets are left
+    	
+    	//Start of placeholder code
+		JLabel paneltext = new JLabel("Freezing, Explosive, Snipe, Misdirection, Snake");
+    	panelArray[11] = new JPanel();
+    	panelArray[11].add(paneltext);
+    	
+    	//End of placeholder Code
+    	
+		frame.setLayout(new BoxLayout(frame.getContentPane(),BoxLayout.Y_AXIS));
+		for(int i=0; i<11; i++){
+			panelArray[i].setBounds(50,50,100, i*70);
+			frame.add(panelArray[i]);
+		}
+		frame.add(panelArray[11]);       
+		
+	}
+	
 }
